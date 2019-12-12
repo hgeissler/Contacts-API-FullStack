e8ac04a7da9e458574df0a6081c2d4f856f9beb1
Hannes <h.geissler@schille.com>
2019-12-12 18:13:37 +0100

    Finalize base server

---
+++ src/main/java/com/crm/crm/model/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,12 @@
+package com.crm.crm.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication {
+  // this main function runs the server 
+  public static void main(String[] args) {
+    SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+  }
+}
